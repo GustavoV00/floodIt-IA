21f68e7afc989cecaf43cdd542fe65f0bcd31962
Gustavo Valente <gustavo.pw00@gmail.com>
2022-03-24 23:22:28 -0300

    add some functions int main

--- src/main.c
+++ src/main.c
@@ -1,1 +1,22 @@
-int main(int argc, char *argv[]) { return 0; }
+#include "../utils/libs/io.h"
+
+int main(int argc, char *argv[])
+{
+	int board_arr[3];
+	FILE *board_file = NULL;
+
+	board_file = open_file(board_file);
+	if (board_file)
+	{
+		read_board_configs(board_arr, board_file);
+		read_board_data(board_arr, board_file);
+		close_file(board_file);
+		// aStar();
+	}
+	else
+	{
+		error_when_open_file(board_file);
+	}
+
+	return 0;
+}
