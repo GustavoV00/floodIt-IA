3a32537b734bce4d5c4ad2e888d8babfc3d650f9
Gustavo Valente <gustavo.pw00@gmail.com>
2022-04-01 12:08:46 -0300

    Working for 100x100 10 colors

--- src/flood_it.c
+++ src/flood_it.c
@@ -3,6 +3,7 @@
 #include "../include/queue.h"
 #include "../include/utils.h"
 #include <assert.h>
+#include <unistd.h>
 
 void print_fila(void *ptr) {
   struct queue_state_t *elem = ptr;
@@ -17,7 +18,7 @@ void print_fila(void *ptr) {
 
 bool goal(struct queue_state_t *queue, int tam) {
   int n = queue_size((queue_t *)queue);
-  printf("Tamanho fila results: %d\n", n);
+  printf("Tamanho fila visited_nodes: %d\n", n);
 
   if (n != tam)
     return false;
@@ -54,13 +55,13 @@ queue_state_t *verify_neighbors(state_t elem, int lin_max, int col_max,
   if (i > 0 && j > 0 && j < col_max - 1 && i < lin_max - 1) {
     // printf("Deu merda aqui\n");
     possible_next =
-        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i][j + 0]);
+        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i][j + 1]);
     possible_next =
-        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i][j - 0]);
+        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i][j - 1]);
     possible_next =
-        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i + 0][j]);
+        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i + 1][j]);
     possible_next =
-        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i - 0][j]);
+        equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i - 1][j]);
     return possible_next;
   }
   if (i == 0) {
@@ -95,14 +96,14 @@ queue_state_t *verify_neighbors(state_t elem, int lin_max, int col_max,
   }
   return possible_next;
 
-  if (j == col_max) {
+  if (j == col_max - 1) {
     possible_next =
         equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i][j - 1]);
     possible_next =
         equals_neighbors(matrix, possible_next, matrix[i][j], matrix[i + 1][j]);
-    if (i > 1)
+    if (i > 0)
       possible_next = equals_neighbors(matrix, possible_next, matrix[i][j],
-                                       matrix[i + 1][j]);
+                                       matrix[i - 1][j]);
 
     return possible_next;
   }
@@ -144,9 +145,9 @@ queue_state_t *find_equals(queue_state_t *f, state_t **m, int i, int j,
   return f;
 }
 
-queue_state_t *search_boards(state_t **matrix, queue_state_t *results, int lin,
-                             int col, queue_state_t *possible_next) {
-  queue_state_t *aux = results;
+queue_state_t *search_boards(state_t **matrix, queue_state_t *visited_nodes,
+                             int lin, int col, queue_state_t *possible_next) {
+  queue_state_t *aux = visited_nodes;
 
   do {
     state_t elem = aux->st;
@@ -156,7 +157,7 @@ queue_state_t *search_boards(state_t **matrix, queue_state_t *results, int lin,
     possible_next = verify_neighbors(elem, lin, col, matrix, possible_next);
 
     aux = aux->next;
-  } while (aux != results);
+  } while (aux != visited_nodes);
 
   return possible_next;
 }
@@ -238,31 +239,34 @@ int *a_star(state_t **matrix_data, int lin, int col, int num_colors) {
 
   // print_matrix(matrix_data, lin, col);
   queue_state_t *possible_next = NULL;
-  queue_state_t *results = NULL;
-  // results = init_elem(matrix_data[0][0], results);
+  queue_state_t *visited_nodes = NULL;
+  int *results = malloc(lin * col * sizeof(int));
+  // visited_nodes = init_elem(matrix_data[0][0], visited_nodes);
 
-  queue_state_t *aux = results;
+  queue_state_t *aux = visited_nodes;
   int flag = 1;
-  results = find_equals(aux, matrix_data, 0, 0, lin, col, flag);
-  // results = find_equals(aux, matrix_data, 1, 0, lin, col, flag);
+  visited_nodes = find_equals(aux, matrix_data, 0, 0, lin, col, flag);
+  // visited_nodes = find_equals(aux, matrix_data, 1, 0, lin, col, flag);
 
-  queue_print("Results: ", (queue_t *)results, print_fila);
+  queue_print("visited_nodes: ", (queue_t *)visited_nodes, print_fila);
   int i = 0;
-  int n = 7;
-  while (!goal(results, lin * col)) {
+  while (!goal(visited_nodes, lin * col)) {
     printf("Jogadas: %d\n", i);
 
     print_matrix(matrix_data, lin, col);
     possible_next =
-        search_boards(matrix_data, results, lin, col, possible_next);
+        search_boards(matrix_data, visited_nodes, lin, col, possible_next);
     // queue_print("PossibleNext: ", (queue_t *)possible_next, print_fila);
-    state_t next_color = chose_next_color(possible_next, lin, col, results);
+    state_t next_color =
+        chose_next_color(possible_next, lin, col, visited_nodes);
+    results[i] = next_color.value;
     // add_next_state_to_board()
     // printf("NextColor: %d\n", next_color.value);
-    results = color_the_board(results, matrix_data, next_color);
+    visited_nodes = color_the_board(visited_nodes, matrix_data, next_color);
 
     possible_next = remove_all_possible_colors(possible_next);
-    results = remove_all_possible_colors(results);
+    // queue_print("visited_nodes: ", (queue_t *)visited_nodes, print_fila);
+    visited_nodes = remove_all_possible_colors(visited_nodes);
     // queue_print("Colors after remove best color: ", (queue_t *)possible_next,
     //             print_fila);
 
@@ -274,14 +278,22 @@ int *a_star(state_t **matrix_data, int lin, int col, int num_colors) {
     // remove_all_possible_colors()
     // state_t *next_elem = calc_heuristic(possible_next);
     // print_matrix(matrix_data, lin, col);
-    aux = results;
-    results = find_equals(aux, matrix_data, 0, 0, lin, col, flag);
-    // queue_print("Results: : ", (queue_t *)results, print_fila);
+    aux = visited_nodes;
+    visited_nodes = find_equals(aux, matrix_data, 0, 0, lin, col, flag);
+    // queue_print("visited_nodes: : ", (queue_t *)visited_nodes, print_fila);
     i += 1;
+    sleep(1);
   }
 
   printf("Jogadas totais: %d\n", i);
   print_matrix(matrix_data, lin, col);
+  printf("\n");
+  printf("Resultado: \n");
+  for (int i = 0; i < lin * col; i++) {
+    if (results[i] != 0)
+      printf("%d ", results[i]);
+  }
+  printf("\n");
 
   return NULL;
 }
