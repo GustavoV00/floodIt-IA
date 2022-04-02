1c64918541c04cd33d36680bc1f5aa9ead0df5ea
Gustavo Valente <gustavo.pw00@gmail.com>
2022-04-02 15:55:47 -0300

    Update main.c
    Remove not used code

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
-//TRAB1 IA - 2021-2
-//GUSTAVO VALENTE NUNES GRR20182557
-//BRUNO EDUARDO FARIAS GRR20186715
+// TRAB1 IA - 2021-2
+// GUSTAVO VALENTE NUNES GRR20182557
+// BRUNO EDUARDO FARIAS GRR20186715
 
 #include "../include/error.h"
 #include "../include/flood_it.h"
@@ -25,14 +25,6 @@ int main(int argc, char *argv[]) {
     // Utilizo uma matriz para fazer a manipulação de estados, e outra com o e
     // tado inicial
     state_t **matrix_data = read_board_data(lin, col, board_file);
-
-    // allocates a auxiliar matrix to hold the initial state
-    state_t **matrix_data_aux = aloc_matrix(matrix_data_aux, lin, col);
-
-    // copy the matrix to this auxiliar matrix
-    matrix_copy(matrix_data, matrix_data_aux, lin, col);
-
-    // print_matrix(matrix_data_aux, lin, col);
 
     // A* algorithm
     int *v = a_star(matrix_data, lin, col, num_colors);
